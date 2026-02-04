63f37ea210adb09a7fd04b2ae42ec55646a29fda
James McGowan <james.j.mcgowan94@gmail.com>
2026-02-03 21:38:04 -0500

    thru 2.5: ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,6 @@
+from multiprocessing import Value
+
+
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -21,14 +24,28 @@ class HTMLNode:
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag=tag, value=value, children=None, props=props)
+        super().__init__(tag, value, None, props=props)
     
     def to_html(self):
         if self.value is None:
-            raise ValueError("LeafNode has no value")
+            raise ValueError("LeafNode must have a value")
         if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
     def __repr__(self):
         return f"Tag: {self.tag}, Value: {self.value}, Props: {self.props}"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if len(self.children) == 0:
+            raise ValueError("ParentNode must have at least one child node")
+        children_html_str = ""
+        for child in self.children:
+            children_html_str += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html_str}</{self.tag}>"
